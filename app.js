c715f100060b8f003fbeac04fdb0dfcac0c3b409
devika <devikavarshney0003@gmail.com>
2023-01-10 22:57:58 +0530

    added header and footer to the home and added the about and contact page

--- app.js
+++ app.js
@@ -16,19 +16,16 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req,res){
-  res.render("home");
+  res.render("home", {StartingContent:homeStartingContent});
 });
 
+app.get("/about", function(req,res){
+  res.render("about", {about:aboutContent});
+});
 
-
-
-
-
-
-
-
-
-
+app.get("/contact", function(req,res){
+  res.render("contact", {contact:contactContent});
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
