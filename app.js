2f8a993e27ff5bb95d8d1aac42249dacf0525be1
Devika Varshney <devikavarshney@Devikas-MacBook-Air-2.local>
2023-06-01 13:53:39 +0530

    connected to mongodb atlas

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const bodyParser = require("body-parser");
 const _ = require("lodash");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+port = 3001;
+mongoose.connect("mongodb+srv://devika:devika06@blogzilla.8l1ygvk.mongodb.net/blogDB", {useNewUrlParser: true,  useUnifiedTopology: true});
 const postSchema = {
   title: String,
   content: String
@@ -89,6 +90,6 @@ app.get("/posts/:postId", function(req, res)
   // });
 });
 
-app.listen(3001, function () {
-  console.log("Server started on port 3001");
+app.listen(process.env.PORT || port, function () {
+  console.log("Server started on port " + port);
 });
